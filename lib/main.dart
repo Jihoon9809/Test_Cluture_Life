72d03dc4a0b7951e82cd26c2c34249fee69c785d
Jihoon9809 <95200335+Jihoon9809@users.noreply.github.com>
2022-04-16 21:56:29 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -24,22 +24,69 @@ class MapSampleState extends State<MapSample> {
   Completer<GoogleMapController> _controller = Completer();
 
   static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
+    target: LatLng(35.1429667, 129.03409),
     zoom: 14.4746,
   );
 
+
   static final CameraPosition _kLake = CameraPosition(
       bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
+      target: LatLng(35.1429667, 129.03409),
       tilt: 59.440717697143555,
       zoom: 19.151926040649414);
 
+
+  List<Marker> _marker = [];
+  final List<Marker> _list = const[
+    Marker(
+        markerId: MarkerId('1'),
+        position: LatLng(35.1429667, 129.03409),
+        infoWindow: InfoWindow(
+          title: 'first position'
+        )
+    ),
+    Marker(
+        markerId: MarkerId('2'),
+        position: LatLng(35.1429667, 129.04409),
+        infoWindow: InfoWindow(
+            title: 'sceond position'
+        )
+    ),
+    Marker(
+        markerId: MarkerId('3'),
+        position: LatLng(35.1429667, 129.02409),
+        infoWindow: InfoWindow(
+            title: 'third position'
+        )
+    ),
+    Marker(
+        markerId: MarkerId('4'),
+        position: LatLng(35.1329667, 129.03409),
+        infoWindow: InfoWindow(
+            title: 'fourth position'
+        )
+    ),
+    Marker(
+        markerId: MarkerId('5'),
+        position: LatLng(35.1529667, 129.03409),
+        infoWindow: InfoWindow(
+            title: 'fifth position'
+        )
+    )
+  ];
+
+  @override
+  void initState(){
+    super.initState();
+    _marker.addAll(_list);
+}
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: GoogleMap(
         mapType: MapType.hybrid,
         initialCameraPosition: _kGooglePlex,
+        markers: Set<Marker>.of(_marker),
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
@@ -57,3 +104,4 @@ class MapSampleState extends State<MapSample> {
     controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
+// 구글지도 마커찍기
